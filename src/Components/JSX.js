71b83a451d1730214b9189e66ba75fe6071b7446
manmeetsingh7781 <manmeet.singh7781@gmail.com>
2022-03-03 23:27:32 -0800

    Finished: React JSX - ways to create HTML tags

--- src/Components/JSX.js
+++ src/Components/JSX.js
@@ -8,12 +8,23 @@ const element = <p>Hello World! - JSX.js</p>
 // React.createElement(tag, attributes, children like text)
 const element2 = React.createElement('p', {id: 'jsx_id'}, 'Created using React.createElement - JSX.js');
 
+
+const navbar = <nav>
+    <h5>Navbar Title</h5>
+    <ul>
+        <li>Docs</li>
+        <li>Examples</li>
+        <li>GitHub</li>
+        <li>Community</li>
+    </ul>
+</nav>
 const page = <div>
                 <hr/>
                 <h1>This component is from JSX.js</h1>
                 <p>This is just a text</p>
                 {element}
                 {element2}  
+                {navbar}
                 <hr/>             
             </div>
 
