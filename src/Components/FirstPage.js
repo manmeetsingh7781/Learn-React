cc05a4d3a19c2595465295574f852bd6d5c5c969
manmeetsingh7781 <manmeet.singh7781@gmail.com>
2022-03-05 00:11:11 -0800

    Completed First Project

--- src/Components/FirstPage.js
+++ src/Components/FirstPage.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import Logo from "../Images/logo.png";
 
+// this is a first challenge from the video
 
 export default function Body(props) {
-    return <h1>Hello World!</h1>
+
+
+    return (
+        <div className="App">
+            <img src={Logo} alt="logo" className="App-logo"/>
+            <h1>Fun facts about React</h1>
+            <ol>
+                <li>Was fist released in 2013</li>
+                <li>Was originally created by Jordan Walkie</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is Maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps.</li>
+            </ol>
+        </div>
+    )
 }
